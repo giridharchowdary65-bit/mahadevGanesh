8c4451e984b1c5f320708065360609d6d7c93035
Giridhar_tarak <giridharchowdary65@gmail.com>
2026-09-07 19:15:37 +0530

    Fix backend and frontend URLs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://mahadevganesh-api.onrender.com/api";
 
 function App() {
 
